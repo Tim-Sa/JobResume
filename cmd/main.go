d549b465a322959d4ddfb1b0d639e87ba24cc0a5
Tim-Sa <salnikovtimofeyof@gmail.com>
2024-03-02 14:42:36 +0300

    structures of employee info

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,51 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type GeneralInfo struct {
+	SNP      string `json:"SNP"`
+	Birthday string `json:"birthday"`
+	About    string `json:"about"`
+	Position string `json:"position"`
+}
+
+type Educations []struct {
+	Type         string `json:"type"`
+	Organisation string `json:"organisation"`
+	Grad         string `json:"grad"`
+	Start        string `json:"start"`
+	End          string `json:"end"`
+	Department   string `json:"department"`
+}
+
+type Contacts struct {
+	Phone      string `json:"phone"`
+	Email      string `json:"email"`
+	GithubLink string `json:"github_link"`
+	City       string `json:"city"`
+	Country    string `json:"country"`
+	Relocation string `json:"relocation"`
+}
+
+type Languages []struct {
+	Name  string `json:"name"`
+	Level string `json:"level"`
+}
+
+type WorkExpiriences struct {
+	Seniority  string `json:"seniority"`
+	Workplaces []struct {
+		Organisation string `json:"organisation"`
+		Position     string `json:"position"`
+		Start        string `json:"start"`
+		End          string `json:"end"`
+		Projects     []struct {
+			Description string `json:"description"`
+			Stack       string `json:"stack"`
+			Comment     string `json:"comment"`
+		} `json:"projects"`
+	} `json:"workplaces"`
+}
+
 var startPage = template.Must(template.ParseFiles("templates/index.html"))
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
